c731ea704e14933419fef8538c13a800550f5bae
Chanwoo Lee <lgw22774@gmail.com>
2025-05-19 10:42:33 +0900

    school lab_1

---
+++ school_labs/list_1lab.py
@@ -0,0 +1,13 @@
+shopping_list = []
+shopping_list.append("milk")
+shopping_list.append("bread")
+shopping_list.append("eggs")
+shopping_list.append("apple")
+
+print(f"현재 쇼핑 리스트:{shopping_list}")
+shopping_list.insert(0, "toilet paper")
+shopping_list.insert(1, "orange juice")
+shopping_list += "chicken", "rice"
+print(f"최종 쇼핑 리스트:{shopping_list}")
+
+
