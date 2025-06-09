1b77795f0eb3f359884a4d46b8aa22ee5528449b
Chanwoo Lee <lgw22774@gmail.com>
2025-06-09 09:58:52 +0900

    10813

---
+++ python_syntax/baekjoon/BOJ_10813.py
@@ -0,0 +1,15 @@
+basket = []
+basket_num, num = map(int,input().split())
+
+for b in range(1,basket_num+1):
+    basket.append(b)
+
+for _ in range(num):
+    index_i, index_j = map(int,input().split())
+    q = basket[index_i-1]
+    basket[index_i-1] = basket[index_j-1]
+    basket[index_j-1] = q
+
+for v in basket:
+    print(v,end=" ")
+    print(v,end=" ")
