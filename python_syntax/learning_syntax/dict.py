e57875e7dd1e229f241c964dcb6161238f4a61c8
Chanwoo Lee <lgw22774@gmail.com>
2025-06-16 11:28:28 +0900

    dict

--- python_syntax/learning_syntax/dict.py
+++ python_syntax/learning_syntax/dict.py
@@ -1,4 +1,4 @@
-bar = {"std1" : 10, "std2" : 20, "std3" : 30}
+# bar = {"std1" : 10, "std2" : 20, "std3" : 30}
 
 # keys = bar.keys()
 
@@ -38,8 +38,38 @@ bar = {"std1" : 10, "std2" : 20, "std3" : 30}
 # for value in bar.values():
 #     print(value)
     
-def prt(**kwargs):
-    for key in kwargs.keys():
-        print(f"key: {key}")
+# def prt(**kwargs):
+#     for key in kwargs.keys():
+#         print(f"key: {key}")
 
-prt(**bar)
+# prt(**bar)
+
+# bar = {'a': 1, 'b': 2, 'c': 3, 'passwd': 1234}
+
+# keys = ['passwd']
+
+# result = {k : v for k, v in bar.items() if k not in keys}
+# print(result)
+
+# bar = ['a','b','c']
+# foo = [10,20,30]
+# pos = [100,200,300,400]
+
+# result = zip(bar,foo,pos)
+
+# print(result)
+
+# for v1,v2,v3 in result:
+#     print(v1,v2,v3)
+    
+bar = ['a','b','c']
+foo = [10,20,30]
+pos = [100,200,300,400]
+
+result = dict(zip(bar,foo))
+
+print(result)
+
+for v1,v2 in result.items():
+    print(v1,v2)
+    
