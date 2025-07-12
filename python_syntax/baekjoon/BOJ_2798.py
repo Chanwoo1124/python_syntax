fa66f197fa70c33ee69363f775d729c53c7daf3b
Chanwoo Lee <lgw22774@gmail.com>
2025-07-12 16:22:57 +0900

    2798

--- python_syntax/baekjoon/BOJ_2798.py
+++ python_syntax/baekjoon/BOJ_2798.py
@@ -1,18 +1,18 @@
 n,m = map(int,input().split())
 l = list(map(int,input().split()))
 
-num = 0
-list = []
+li = []
 
 for l_v in range(len(l)):
     for l_i in range(l_v+1,len(l)):
-        for l_i_i in range(l_v+2,len(l)):
-            list.append(l[l_v]+l[l_i]+l[l_i_i])
-print(list)
+        for l_i_i in range(l_i+1,len(l)):
+            li.append(l[l_v]+l[l_i]+l[l_i_i])
 
-for val in list:
+num = 0
+for val in li:
     if num < val <= m:
         num = val
     
 print(num)
+print()
         
